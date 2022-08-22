ea49dc9e2c0d1b02c151f60bb8342c49a46ab4c5
Losemanki6656 <bloggerg420@gmail.com>
2022-08-22 08:58:11 +0500

    asd

--- resources/views/admin/users.blade.php
+++ resources/views/admin/users.blade.php
@@ -40,57 +40,59 @@
                                     </div>
                                 </form>
                             </div>
-                    </div>
+                        </div>
 
-                    <div class="table-responsive mt-1">
-                        <table class="table table-bordered table-striped table-sm">
-                            <thead class="thead-dark">
-                                <tr>
-                                    <th class="text-center fw-bold">#</th>
-                                    <th class="text-center fw-bold">Foydalanuvchi nomi</th>
-                                    <th class="text-center fw-bold">Korxona nomi</th>
-                                    <th class="text-center fw-bold">Email</th>
-                                    <th class="text-center fw-bold">Password</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                @foreach ($users as $item)
+                        <div class="table-responsive mt-1">
+                            <table class="table table-bordered table-striped table-sm">
+                                <thead class="thead-dark">
                                     <tr>
-                                        <td class="text-center fw-bold">
-                                            {{ $users->currentPage() * 15 - 15 + $loop->index + 1 }}</td>
-                                        <td class="text-center fw-bold" style="font-size: 14px">
-                                            {{ $item->name }}</td>
-                                        <td>{{ $item->userorganization->organization->name ?? ''}}</td>
-                                        <td>{{ $item->email }}</td>
-                                        <td>
-                                           @if (Auth::user()->id == 1 ||  Auth::user()->id == 239)
-                                               @if ($item->userorganization->post_id == 1)
-                                                   user12345
-                                               @else
-                                                   {{ $item->userorganization->post_id }}
-                                               @endif
-                                           @else
-                                               Password
-                                           @endif
-                                        </td>
+                                        <th class="text-center fw-bold">#</th>
+                                        <th class="text-center fw-bold">Foydalanuvchi nomi</th>
+                                        <th class="text-center fw-bold">Korxona nomi</th>
+                                        <th class="text-center fw-bold">Email</th>
+                                        <th class="text-center fw-bold">Password</th>
                                     </tr>
-                                @endforeach
-                            </tbody>
-                        </table>
-                    </div>
-                    <div class="d-flex justify-content-center mt-3">
-                        <ul class="pagination mb-0">
-                            {{ $users->withQueryString()->links() }}
-                        </ul>
-                    </div>
+                                </thead>
+                                <tbody>
+                                    @foreach ($users as $item)
+                                        <tr>
+                                            <td class="text-center fw-bold">
+                                                {{ $users->currentPage() * 15 - 15 + $loop->index + 1 }}</td>
+                                            <td class="text-center fw-bold" style="font-size: 14px">
+                                                {{ $item->name }}</td>
+                                            <td>{{ $item->userorganization->organization->name ?? '' }}</td>
+                                            <td>{{ $item->email }}</td>
+                                            <td>
+                                                @if (Auth::user()->id == 1 || Auth::user()->id == 239)
+                                                    @if ($item->userorganization)
+                                                        @if ($item->userorganization->post_id == 1)
+                                                            user12345
+                                                        @else
+                                                            {{ $item->userorganization->post_id }}
+                                                        @endif
+                                                    @endif
+                                                @else
+                                                    Password
+                                                @endif
+                                            </td>
+                                        </tr>
+                                    @endforeach
+                                </tbody>
+                            </table>
+                        </div>
+                        <div class="d-flex justify-content-center mt-3">
+                            <ul class="pagination mb-0">
+                                {{ $users->withQueryString()->links() }}
+                            </ul>
+                        </div>
 
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
 
 
-@endsection
+    @endsection
 
-@section('scripts')
-@endsection
+    @section('scripts')
+    @endsection
