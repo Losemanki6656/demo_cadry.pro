3ad6931c90467731f50ebbbbae8c0bc7e99470cb
Losemanki6656 <bloggerg420@gmail.com>
2022-10-18 19:07:37 +0500

    Asd

--- app/Http/Resources/StaffOrgResource.php
+++ app/Http/Resources/StaffOrgResource.php
@@ -4,6 +4,7 @@ namespace App\Http\Resources;
 
 use App\Models\DepartmentStaff;
 use App\Models\Department;
+use App\Models\DepartmentCadry;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class StaffOrgResource extends JsonResource
@@ -18,11 +19,12 @@ class StaffOrgResource extends JsonResource
     {
         $ids = DepartmentStaff::where('staff_id', $this->id)->groupBy('department_id')->pluck('department_id')->toArray();
         $deps = Department::whereIn('id',$ids)->get();
+        $cadries_count = DepartmentCadry::where('staff_id', $this->id)->count();
         return [
             'id' => $this->id,
             'name' => $this->name,
             'category_id' => $this->category_id,
-            'cadries_count' => $this->cadries->count(),
+            'cadries_count' => $cadries_count,
             'departments_count' => $deps->count(),
             'departments' => DepResource::collection($deps)
         ];
