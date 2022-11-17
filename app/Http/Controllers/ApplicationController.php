7abfd851cf976fdfe41574cf8fcffb49ceff7c1b
Losemanki6656 <bbiznestg@gmail.com>
2022-11-18 01:35:58 +0500

    Asd

--- app/Http/Controllers/ApplicationController.php
+++ app/Http/Controllers/ApplicationController.php
@@ -3,15 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Cadry;
 
 class ApplicationController extends Controller
 {
     
     public function find_cadry(Request $request)
     {
+        $passport = str_replace('-', ' ', $request->passport);
+        $jshshir = str_replace('-', '', $request->jshshir);
 
-        return response()->json([
-            'data' => $request->all()
-        ]);
+        $cadry = Cadry::where('birht_date', $request->birht_date)->where('jshshir', $jshshir)->where('passport', $passport)->first()->id;
+
+        return app(\App\Http\Controllers\OrganizationController::class)->word_export_api($cadry);
     }
 }
