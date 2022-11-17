b521a7a3040ae61091b5a419623653ba7927b1d9
Losemanki6656 <bbiznestg@gmail.com>
2022-11-18 01:40:39 +0500

    Asd

--- app/Http/Controllers/ApplicationController.php
+++ app/Http/Controllers/ApplicationController.php
@@ -13,8 +13,13 @@ class ApplicationController extends Controller
         $passport = str_replace('-', ' ', $request->passport);
         $jshshir = str_replace('-', '', $request->jshshir);
 
-        $cadry = Cadry::where('birht_date', $request->birht_date)->where('jshshir', $jshshir)->where('passport', $passport)->first()->id;
+        $cadry = Cadry::where('status', true)->where('birht_date', $request->birht_date)->where('jshshir', $jshshir)->where('passport', $passport)->first();
 
-        return app(\App\Http\Controllers\OrganizationController::class)->word_export_api($cadry);
+        if(!$cadry) {
+            return response()->json([
+                'message' => "Exodim Platformasida bunday xodim topilmadi yoki Platformadagi ma'lumotlarga siz to'ldirgan ma'lumotlar mos kelmadi..."
+            ],403);
+        } else
+        return app(\App\Http\Controllers\OrganizationController::class)->word_export_api($cadry->id);
     }
 }
