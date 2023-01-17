169fba8a6e8a3e1c60c99c2ba983406cde05d40d
Losemanki6656 <bbiznestg@gmail.com>
2023-01-17 10:15:14 +0500

    Asd

--- app/Models/Cadry.php
+++ app/Models/Cadry.php
@@ -271,7 +271,7 @@ class Cadry extends Model
                 return $query->where('last_name', 'LIKE', '%'. $last_name .'%');
                 
             })->when(request('middle_name'), function ( $query, $middle_name) {
-                return $query->where('middle_name', 'LIKE', '%'. $middle_name .'%');
+                return $query->where('post_name', 'LIKE', '%'. $middle_name .'%');
                 
             })->when(request('first_name'), function ( $query, $first_name) {
                 return $query->where('first_name', 'LIKE', '%'. $first_name .'%');
