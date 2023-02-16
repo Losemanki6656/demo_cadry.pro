713e1ca4d660934f1aafb9b692938beb2947bd09
Losemanki6656 <bbiznestg@gmail.com>
2023-02-16 09:47:33 +0500

    sad

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,15 @@ use App\Http\Controllers\TechnicalSchoolController;
 //Auth::routes();
 Auth::routes(['register' => false]);
 
+Route::get('/migrate', function () {
+   
+    Schema::disableForeignKeyConstraints();
+
+    Artisan::call('migrate');
+           Schema::enableForeignKeyConstraints();
+
+});
+
 Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
