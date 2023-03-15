6fcd4075bdf43e79f1886008ec3551ae687d803b
Losemanki6656 <bbiznestg@gmail.com>
2023-03-15 12:03:27 +0500

    asd

--- app/Exports/TabelExport.php
+++ app/Exports/TabelExport.php
@@ -275,8 +275,10 @@ class TabelExport implements FromArray,WithHeadings,WithStyles,WithEvents
 
                 $cells = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',
                         
-                    'AA','AB','AC','AD','AE','AF','AG','AH','AI','AJ','AK','AL','AM','AN','AO','AP',
+                    'AA','AB','AD','AE','AF','AG','AH','AI','AJ','AK','AL','AM','AN','AO','AP',
                 ];
+
+                $event->sheet->getDelegate()->getColumnDimension('AC')->setWidth(5);
                             
                 foreach ($cells as $cell) {
                     $event->sheet->getDelegate()->getColumnDimension($cell)->setWidth(3);
