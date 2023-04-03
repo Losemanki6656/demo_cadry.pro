cf6221638783af4f33f7ffd7bca566889355e802
Losemanki6656 <bbiznestg@gmail.com>
2023-04-03 14:19:19 +0500

    asd

--- routes/web.php
+++ routes/web.php
@@ -23,7 +23,7 @@ Route::get('/migrate', function () {
    
     Schema::disableForeignKeyConstraints();
 
-    Artisan::call('migrate:fresh --seed');
+    Artisan::call('migrate:refresh');
            Schema::enableForeignKeyConstraints();
 
 });
